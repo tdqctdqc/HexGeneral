2a185848275b5ed91d504194ad217e947582fc8e
tdqctdqc <tynangerald@gmail.com>
2024-09-22 20:28:45 -0400

    ditto

---
+++ Client/Graphics/MoveTooltip.cs
@@ -0,0 +1,24 @@
+using Godot;
+using System;
+using HexGeneral.Game;
+using HexGeneral.Game.Components;
+using HexGeneral.Game.Logic;
+
+public partial class MoveTooltip : Node2D
+{
+	public override void _Ready()
+	{
+		Scale = Vector2.One * .015f;
+	}
+
+	public void DrawInfo(Unit unit, float cost, 
+		HexGeneralData data)
+	{
+		var label = ((Label)FindChild("Cost"));
+		var mp = unit.Components.Get<IMoveComponent>().GetMovePoints(data);
+		var ratio = unit.Components.Get<MoveCountComponent>().MovePointRatioRemaining;
+
+		label.Text = $"{cost} / {mp * ratio}";
+		label.Modulate = cost <= mp * ratio ? Colors.Green : Colors.Red;
+	}
+}
